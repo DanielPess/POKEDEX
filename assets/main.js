201e06a960546947f085d92bba7c406b35a2b294
DanielPess <daniel.amorim.br10@gmail.com>
2023-09-22 18:30:28 -0400

    alterações

--- assets/main.js
+++ assets/main.js
@@ -1,34 +1,35 @@
 
 
-const offset= 0
-const limit= 10
-const url= 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 function convertPokemonToHtml(pokemon) {
-    return  
-    <li class="pokemon"> 
-        <span class="number">#001</span>
-        <span class="name">${pokemon.name}</span>
-
-        <div class="detail">
-            <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poison</li>
-            </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"alt="${pokemon.name}">
-        </div>
-    </li>
-    
+    return (
+        `<li class="pokemon"> 
+            <span class="number">#${pokemon.id.toString().padStart(3, "0")}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map(type => `<li class="type">${type.type.name}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+            </div>
+        </li>`
+    );
 }
-
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) =>jsonBody.results)
+    .then((jsonBody) => jsonBody.results)
     .then((pokemonList) => {
-        for (let i=0;i<pokemonList.lenght; i++){
-            const pokemon= pokemonList[1];
-            console.log(convertPokemonToLi(pokemon))
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemonUrl = pokemonList[i].url;
+            fetch(pokemonUrl)
+                .then((response) => response.json())
+                .then((pokemonData) => {
+                    console.log(convertPokemonToHtml(pokemonData));
+                })
+                .catch((error) => console.error(error));
         }
     })
-    .catch((error) => console.error(error))
-    
+    .catch((error) => console.error(error));
